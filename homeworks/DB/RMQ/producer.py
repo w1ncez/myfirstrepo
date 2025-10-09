cfee8dfe3ccfc5245d15f405c5081f033867a086
Sergey Wince <w1nce@yandex.ru>
2025-10-09 19:25:57 +0300

    Update producer.py

--- homeworks/DB/RMQ/producer.py
+++ homeworks/DB/RMQ/producer.py
@@ -2,7 +2,7 @@
 # coding=utf-8
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.1.8'))
 channel = connection.channel()
 channel.queue_declare(queue='hello')
 channel.basic_publish(exchange='', routing_key='hello', body='Hello Netology!')
